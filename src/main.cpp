1ee7eac1fa0ad23f8b75b628ce0800dd0c0c5522
ACB-prgm <63984796+ACB-prgm@users.noreply.github.com>
2022-06-15 23:40:34 -0400

    Encoders

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,62 @@
 #include <Arduino.h>
 
 
-const int MOTORS[3][4] = {
-  {7, 4, 48, 49}, // MOTOR 1 | PHASE | ENABLE | ENC-A | ENC-B
-  {6, 3, 50, 51}, // MOTOR 2 | PHASE | ENABLE | ENC-A | ENC-B
-  {5, 2, 52, 53}, // MOTOR 3 | PHASE | ENABLE | ENC-A | ENC-B
+int MOTORS[3][6] = {
+  {7, 4, 48, 49, 0, 0}, // MOTOR 1, PHASE_PIN, EN_PIN, ENC-A, ENC-B, DIRECTION, NUM_PULSES  | ANGLEº = (NUM_PULSES /  PPR) * 360º
+  {6, 3, 50, 51, 0, 0},
+  {5, 2, 52, 53, 0, 0},
 };
+const int test_motor = 0;
+const int PPR = 4560;
 
 
 void run_motor(int motor_pin, int dir, int pwm_val) {
   analogWrite(MOTORS[motor_pin][1], pwm_val);
   digitalWrite(MOTORS[motor_pin][0], dir);
 
+  MOTORS[motor_pin][4] = dir;
+}
+
+
+void record_pulses() {
+  static int last_encs[3] = {3, 3, 3};
+
+  int encs[3][2] = {
+    {digitalRead(MOTORS[0][2]), digitalRead(MOTORS[0][3])},
+    {digitalRead(MOTORS[1][2]), digitalRead(MOTORS[1][3])},
+    {digitalRead(MOTORS[2][2]), digitalRead(MOTORS[2][3])}
+    };
+  
+  for (int i=0; i<3; i++) {
+    int last_enc = last_encs[i];
+    int enc = encs[i][0] + encs[i][1];
+
+    if (enc != last_enc) {
+      MOTORS[i][5] += 1;
+      last_encs[i] = enc;
+    };
+    Serial.print(MOTORS[i][5]);
+    Serial.print(" | ");
+  }
+
+  Serial.println();
 }
 
 
 void setup() {
   Serial.begin(250000); // MUST CHANGE IN .ini FILE TO VIEW IN SERIAL MONITOR
+  run_motor(0, 0, 40);
 }
 
-int count = 0;
-void loop() {
-  if(0){
-    static int motor = 0;
-    static bool hell = false;
-    run_motor(motor, 0, 40);
 
-    int enc[2] = {digitalRead(MOTORS[motor][2]), digitalRead(MOTORS[motor][3])};
-    if (count < 201 && (enc[0] + enc[1] == 2 || hell)){
-      hell = true;
-      count += 1;
-      Serial.println(enc[0] + enc[1]);
+int RUN = 1;
+
+void loop() {
+  if(RUN){
+    record_pulses();
+
+    if (MOTORS[test_motor][5] >= PPR){
+      analogWrite(MOTORS[test_motor][1], 0);
     }
   }
 }
-
-
-// int interpret_enc(int motor) {
-//   return MOTORS[motor][]
-// }

2b929cfc79be106917c2dc0c26425b4a10de66ac
ACB-prgm <63984796+ACB-prgm@users.noreply.github.com>
2022-06-16 12:58:44 -0400

    Fin encoders
    max pwm is 50

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,32 @@
 #include <Arduino.h>
 
 
+// GLOBALS
+#define RUN 1
+#define PPR 4560.0 
+#define cm_PER_ROT 6.28
+#define MAX_SPEED 25  // THE MAX SPEED THE ENCODER CAN ~ACCURATELY READ AT
+#define test_motor 0
+
 int MOTORS[3][6] = {
-  {7, 4, 48, 49, 0, 0}, // MOTOR 1, PHASE_PIN, EN_PIN, ENC-A, ENC-B, DIRECTION, NUM_PULSES  | ANGLEº = (NUM_PULSES /  PPR) * 360º
+  {7, 4, 48, 49, 0, 0}, // MOTOR 1, PHASE_PIN, EN_PIN, ENC-A, ENC-B, DIRECTION, NUM_PULSES
   {6, 3, 50, 51, 0, 0},
   {5, 2, 52, 53, 0, 0},
 };
-const int test_motor = 0;
-const int PPR = 4560;
 
 
+// CUSTOM FUNCTIONS
 void run_motor(int motor_pin, int dir, int pwm_val) {
+  // MOVES MOTOR IN DIRECTION
   analogWrite(MOTORS[motor_pin][1], pwm_val);
   digitalWrite(MOTORS[motor_pin][0], dir);
 
   MOTORS[motor_pin][4] = dir;
 }
 
-
 void record_pulses() {
+  // RECORDS THE NUMBER OF PULSES FROM ENCODERS TO EACH MOTOR IN MOTORS
+  // PULSES CAN THEN BE USED TO DETERMINE THE NUMBER OF ROTATIONS, DISTANCE, AND ANGLE
   static int last_encs[3] = {3, 3, 3};
 
   int encs[3] = {
@@ -32,31 +40,50 @@ void record_pulses() {
     int enc = encs[i];
 
     if (enc != last_enc) {
-      MOTORS[i][5] += 1;
+      MOTORS[i][5] += 1 - (2 * MOTORS[i][4]); // left rotation decrements, right increments pulses
       last_encs[i] = enc;
     };
-    Serial.print(MOTORS[i][5]);
-    Serial.print(" | ");
   }
 
   Serial.println();
 }
 
+float get_motor_rotations(int motor_pin) {
+  // RETURNS THE NUMBER OF REVOLUTIONS FROM THE ORIGIN 
+  return (MOTORS[motor_pin][5] /  PPR); // (NUM_PULSES / PPR)
+}
 
-void setup() {
-  Serial.begin(250000); // MUST CHANGE IN .ini FILE TO VIEW IN SERIAL MONITOR
-  run_motor(0, 0, 40);
+float get_motor_angle(int motor_pin) {
+  // RETURNS THE ROTATION ANGLE FROM THE ORIGIN
+  return get_motor_rotations(motor_pin) * 360;  // NUM_ROTATIONS * 360
+}
+
+float get_motor_dist(int motor_pin){
+  // RETURNS THE DISTANCE (cm) FROM THE ORIGIN THE "MOTOR" HAS TRAVELED (IE THE ROD IN OUR CASE)
+  return get_motor_rotations(motor_pin) * cm_PER_ROT; // NUM_ROTATIONS * ~CIRCUMFERENCE
 }
 
 
-int RUN = 1;
+// VIRTUAL FUNCTIONS
+void setup() {
+  Serial.begin(2000000); // MUST CHANGE IN .ini FILE TO VIEW IN SERIAL MONITOR
+  if (RUN) {
+    run_motor(test_motor, 0, MAX_SPEED);
+  }
+}
+
 
 void loop() {
   if(RUN){
+
     record_pulses();
 
     if (MOTORS[test_motor][5] >= PPR){
       analogWrite(MOTORS[test_motor][1], 0);
+      exit(0);
     }
+    Serial.print(get_motor_angle(test_motor));
+    Serial.print("º | cm = ");
+    Serial.println(get_motor_dist(test_motor));
   }
 }
